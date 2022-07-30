499c12b42575b06c4971e07ec4be8ac822e31490
Manu Shekhar <86350276+iammanuss@users.noreply.github.com>
2022-07-30 18:40:39 +0530

    Time: 4 ms (40.43%), Space: 5.8 MB (86.48%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n == 0) return false;
+        while(true) {
+            if(n == 1) return true;
+            if(n % 5 == 0)
+                n = n / 5;
+            else if(n % 3 == 0)
+                n = n / 3;
+            else if(n % 2 == 0)
+                n = n / 2;
+            else return false;
+        }
+    }
+};
